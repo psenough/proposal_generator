e655bd7fa2e7edd9293c7ecdda902207f6d6834e
Filipe Cruz <psenough@gmail.com>
2014-11-29 07:24:46 +0200

    operator add days

--- js/index.js
+++ js/index.js
@@ -4,8 +4,9 @@ var Character = function(name, image) {
 	this.image = image;
 };
 
-var Keyword = function(name) {
+var Keyword = function(name, operators) {
 	this.name = name;
+	this.operators = operators;
 	this.toggled = false;
 };
 
@@ -24,9 +25,20 @@ var PG = function() {
 	this.selectedCharacter = -1;
 	
     this.keywords = [
-    	new Keyword('web'),
-    	new Keyword('desktop'),
-    	];
+		new Keyword( 'web',
+				[
+					{'target':'specification', 'type':'add', 'value':5},
+					{'target':'database', 'type':'add', 'value':5},
+					{'target':'frontend design', 'type':'add', 'value':5},
+					{'target':'frontend development', 'type':'add', 'value':10},
+					{'target':'backoffice development', 'type':'add', 'value':10},
+					{'target':'backoffice design', 'type':'add', 'value':5}
+				]
+		),				
+		new Keyword( 'desktop',
+				[{'target':'frontend development', 'type':'add', 'value':30}]
+		),
+	];
     	
     this.eurosHour = 35;
     this.hoursDay = 7;
@@ -58,7 +70,7 @@ PG.prototype = {
 		var dom = document.getElementById('leftdivkeywords');
 		if (dom) {
 			dom.innerHTML = '';
-			console.log(this.keywords);
+			//console.log(this.keywords);
 			for (var i=0; i<this.keywords.length; i++) {
 				
 				// add keyword to div
@@ -109,7 +121,11 @@ PG.prototype = {
 			var budget = document.createElement('div');
 			budget.setAttribute('id', 'budget');
 			var output = '<table>';
-			
+			this.processKeywords();
+			console.log(this.budget_software);
+			for (key in this.budget_software) {
+				output += '<tr><td>'+key+'</td><td>'+this.budget_software[key]+'</td>';
+			}
 			output += '</table>';
 			
 			budget.innerHTML = output;
@@ -121,6 +137,56 @@ PG.prototype = {
 		
 		}
 		
+	},
+	
+	initBudgets: function() {
+		this.budget_software = [];
+		this.budget_hardware = [];
+	},
+	
+	budgetFunction: function(array, operator) {
+		var property = operator['target'];
+		var operation = operator['type'];
+		var value = operator['value'];
+		
+		switch(operation) {
+			case 'add':
+				if (array.hasOwnProperty(property)) {
+					array[property] += value;
+				} else {
+					array[property] = value;
+				}
+			break;
+			case 'mul':
+				if (array.hasOwnProperty(property)) array[property] *= value;
+				//	else array[property] = 0;
+			break;
+		}			
+	},
+	
+	processKeywords: function() {
+		this.initBudgets();
+		
+		for(var i=0; i<this.keywords.length; i++) {
+			
+			// pass for adding values
+			for(var j=0; j<this.keywords[i].operators.length; j++) {
+				if (this.keywords[i].operators[j]['type'] == 'add')
+					this.budgetFunction(
+						this.budget_software,
+						this.keywords[i].operators[j]
+						);
+			}
+			
+			// pass for multiplying values
+			for(var j=0; j<this.keywords[i].operators.length; j++) {
+				if (this.keywords[i].operators[j]['type'] == 'mul')
+					this.budgetFunction(
+						this.budget_software,
+						this.keywords[i].operators[j]
+						);
+			}
+		}
 	},
 	
 	characterIs: function(type) {
